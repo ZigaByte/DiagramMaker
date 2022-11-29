8a38e414d1dbf10d92103f93e7b5cde78bca97f5
Ziga Vene <ziga.vene@gmail.com>
2022-11-29 16:16:16 +0100

    Menu for windows

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -181,10 +181,6 @@ export default class MenuBuilder {
         label: '&File',
         submenu: [
           {
-            label: '&Open',
-            accelerator: 'Ctrl+O',
-          },
-          {
             label: '&Close',
             accelerator: 'Ctrl+W',
             click: () => {
@@ -194,46 +190,45 @@ export default class MenuBuilder {
         ],
       },
       {
-        label: '&View',
-        submenu:
-          process.env.NODE_ENV === 'development' ||
-          process.env.DEBUG_PROD === 'true'
-            ? [
-                {
-                  label: '&Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  },
-                },
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  },
-                },
-                {
-                  label: 'Toggle &Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.webContents.toggleDevTools();
-                  },
-                },
-              ]
-            : [
-                {
-                  label: 'Toggle &Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  },
-                },
-              ],
+        label: '&Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Ctrl+Z',
+            click: () => {
+              this.mainWindow.webContents.send('menu-undo');
+            },
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Ctrl+Z',
+            click: () => {
+              this.mainWindow.webContents.send('menu-redo');
+            },
+          },
+          { type: 'separator' },
+          {
+            label: 'Add Node',
+            accelerator: 'A',
+            click: () => {
+              this.mainWindow.webContents.send('menu-add-node');
+            },
+          },
+          {
+            label: 'Connect Nodes',
+            accelerator: 'C',
+            click: () => {
+              this.mainWindow.webContents.send('menu-connect');
+            },
+          },
+          {
+            label: 'Delete',
+            accelerator: 'D',
+            click: () => {
+              this.mainWindow.webContents.send('menu-delete');
+            },
+          },
+        ],
       },
     ];
 
